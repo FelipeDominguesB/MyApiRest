13b7826955b763b42eb6ccb536cd3d529835b529
FelipeDominguesB <felipedom2001@hotmail.com>
2021-02-11 16:01:24 -0300

    Adição do neDB e Insert com POST

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
+
 module.exports = (app) =>{
 
     app.get('/usuarios', (request, response) =>{
@@ -10,14 +16,23 @@ module.exports = (app) =>{
             {nome: 'Julia', idade: 19},
             {nome: 'Arthur', idade: 17},
         ]
+    });
+    });
 
-    })
-});
-
-app.post('/usuarios', (request, response) =>{
-    
-
-    response.json(request.body);
-});
+    app.post('/usuarios', (request, response) =>{
+        
+        db.insert(request.body, (err, usuario) =>{
+            if(err) {
+                console.log(err);
+                response.status(400).json({
+                    error: err,
+                });
+            }
+            else
+            {
+                response.status(200).json(usuario);
+            }
+        });
+    });
 
 };
